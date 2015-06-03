c08c6548b90b818acfa7e111932c3ea9443f0041
kiemrong08 <minimogdiablos@gmail.com>
2015-06-03 17:13:33 +0700

    Added views to create/edit blog post inside the site.

---
+++ daycode.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd "D:\03. Python\djangogirls"
+git add .
+git commit -am "made changes"
+git push
+echo Press Enter...
+read
---
+++ push.bat
@@ -0,0 +1,6 @@
+#ECHO OFF
+git status
+git add -A .
+git status
+git commit -m "Added views to create/edit blog post inside the site."
+git push
