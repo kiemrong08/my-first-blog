61e4741ab176c62a0604ebed7a81d4369b3d1a2e
kiemrong08 <minimogdiablos@gmail.com>
2015-10-16 17:24:42 +0700

    Them rango

--- mysite/rango/views.py
+++ mysite/rango/views.py
@@ -5,11 +5,14 @@ def index(request):
 	#return HttpResponse("Rango says: Hello world! <br/> <a href='/rango/about'>About</a>")
 	#Xây dựng từ điển để truyền template engine như là context.
 	#Chú ý khóa boldmessage giống hệt {{boldmessage}} trong template.
-	context_dict={'boldmessage': "I am bold font from context"}
+	context_dict={'boldmessage': "TÔI LÀ RANGO !!!"}
 
 	#Trả lại phản hồi đã được render để gửi đến client.
 	#Tạo một hàm shortcut để khiến dễ dàng hơn.
 	#Tham số đầu tiên phải là template định dùng.
 	return render(request, 'rango/index.html', context_dict)
 def about(request):
-	return HttpResponse("Rango says here is the <a href='/rango/'>Index</a> page.")
+	return HttpResponse("Trở về trang <a href='/rango/'>chủ</a>")
+
+def about_phu(request):
+	return render(request,'rango/about.html')
